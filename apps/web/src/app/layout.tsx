f1b6d299c816609ed56b0b73bbeaa734e60649db
Aditya Pratap Singh <104914040+haki-user@users.noreply.github.com>
2024-09-16 15:16:50 +0530

    Revert "AWS RUM setup for monitoring"

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -4,37 +4,11 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { Navbar } from "@/components/navbar";
 import AuthProvider from "@/context/auth-context";
-import { AwsRum, AwsRumConfig } from 'aws-rum-web';
 
 export const metadata: Metadata = {
   title: "Ryuk",
   description: "AI Companion",
 };
-
-
-try {
-  const config: AwsRumConfig = {
-    sessionSampleRate: 1,
-    identityPoolId: "us-east-1:ab346f52-2a6f-45a8-a3ed-db97f1734c1d",
-    endpoint: "https://dataplane.rum.us-east-1.amazonaws.com",
-    telemetries: ["performance","errors","http"],
-    allowCookies: true,
-    enableXRay: false
-  };
-
-  const APPLICATION_ID: string = '30974108-bf59-4372-92a2-d2e7065a739f';
-  const APPLICATION_VERSION: string = '1.0.0';
-  const APPLICATION_REGION: string = 'us-east-1';
-
-  const awsRum: AwsRum = new AwsRum(
-    APPLICATION_ID,
-    APPLICATION_VERSION,
-    APPLICATION_REGION,
-    config
-  );
-} catch (error) {
-  // Ignore errors thrown during CloudWatch RUM web client initialization
-}
 
 export default function RootLayout({
   children,
